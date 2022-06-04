58a6ee84d7fd331c594d8cfa122e6f8a4ace2ea3
maticamisay <maticamisay99@hotmail.com>
2022-06-04 20:51:46 -0300

    setting routing

---
+++ src/components/destination.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const destination = () => {
+  return (
+    <div>
+        <h1>Destination</h1>
+    </div>
+  )
+}
+
+export default destination
---
+++ src/components/navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const navbar = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/destination"}>Destination</Link>
+        </li>
+        <li>
+          <Link to={"/crew"}>Crew</Link>
+        </li>
+        <li>
+          <Link to={"/technology"}>Technology</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default navbar;
